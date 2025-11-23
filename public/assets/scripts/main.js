b2cdb8265db7bc61102c109d218dee80f5a303ff
Leotens <leotens11@gmail.com>
2025-11-23 12:26:24 -0500

    feat: agregar animacion a sidebar

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -293,3 +293,21 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
+
+const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".navbar-right");
+
+    if (hamburger && navMenu) {
+        hamburger.addEventListener("click", () => {
+            // Alternar clase 'active' en el icono y el menú
+            hamburger.classList.toggle("active");
+            navMenu.classList.toggle("active");
+        });
+
+        // Cerrar el menú al hacer clic en cualquier enlace dentro del menú
+        document.querySelectorAll(".navbar-right a").forEach(n => n.addEventListener("click", () => {
+            hamburger.classList.remove("active");
+            navMenu.classList.remove("active");
+        }));
+    }
